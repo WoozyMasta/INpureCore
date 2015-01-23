34a0ded1ce515b2b7432039a8596d4df3e36dbe4
Mike <mmelvin0@gmail.com>
2015-01-22 21:14:37 -0800

    Treat buildcraft.transport.ItemFacade.allFacades as AbstractList instead of LinkedList (fixes #5)

--- src/main/java/info/inpureprojects/core/NEI/gtfoMicroblocks/ScriptObjects/BCObject.java
+++ src/main/java/info/inpureprojects/core/NEI/gtfoMicroblocks/ScriptObjects/BCObject.java
@@ -5,8 +5,8 @@ import info.inpureprojects.core.INpureCore;
 import info.inpureprojects.core.NEI.gtfoMicroblocks.NEIINpureConfig;
 import net.minecraft.item.ItemStack;
 
+import java.util.AbstractList;
 import java.util.Arrays;
-import java.util.LinkedList;
 
 /**
  * Created by den on 10/29/2014.
@@ -20,7 +20,7 @@ public class BCObject {
     public void obliterate_facades(int index) {
         NEIINpureConfig.logger.debug("obliterate_microblocks called (version in %s). Params: %s", this.getClass().getName(), String.valueOf(index));
         try {
-            LinkedList<ItemStack> facades = (LinkedList) Class.forName("buildcraft.transport.ItemFacade").getDeclaredField("allFacades").get(null);
+            AbstractList<ItemStack> facades = (AbstractList<ItemStack>)Class.forName("buildcraft.transport.ItemFacade").getDeclaredField("allFacades").get(null);
             API.setItemListEntries(facades.get(0).getItem(), Arrays.asList(new ItemStack[]{facades.get(index)}));
         } catch (Throwable t) {
             INpureCore.proxy.warning("Failed to hook bc!");
@@ -30,7 +30,7 @@ public class BCObject {
 
     public int getFacadesSize() {
         try {
-            LinkedList<ItemStack> facades = (LinkedList) Class.forName("buildcraft.transport.ItemFacade").getDeclaredField("allFacades").get(null);
+            AbstractList<ItemStack> facades = (AbstractList<ItemStack>)Class.forName("buildcraft.transport.ItemFacade").getDeclaredField("allFacades").get(null);
             NEIINpureConfig.logger.debug("getFacadesSize called. Returned: %s", String.valueOf(facades.size()));
             return facades.size();
         } catch (Throwable t) {
